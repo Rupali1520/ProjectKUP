fd0033e9d66dc728baed3bed1c14160e6d9ab1a6
Rupali Gupta <126675784+Rupali1520@users.noreply.github.com>
2023-05-03 21:08:16 +0530

    Update settings.py

--- ParkTicketManagement/settings.py
+++ ParkTicketManagement/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ng&w0xund-l3m-#jldun$6%n^i2o*a5(1e!b)4@j(v(=pj2&(c'
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
